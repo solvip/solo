77bf55c18e7654811a7e02ea0c0af25e827ab047
Sölvi Páll Ásgeirsson <solvip@gmail.com>
2016-12-24 22:55:30 +0000

    Increase test coverage

--- test/solo_tests.erl
+++ test/solo_tests.erl
@@ -15,39 +15,58 @@ start() ->
 stop(_) ->
     solo:stop().
 
-simple_call_test_() ->
-    {"simple calls are correctly handled",
-     ?setup(fun() ->
-                    ?assertEqual(value, solo:call(key, fun() -> value end)),
-                    ?assertNotEqual(false, solo:call(key, fun() -> true end))
-            end)}.
-
-duplicate_call_test_() ->
-    {"duplicate calls are suppressed",
-     ?setup(fun duplicate_suppression/0)}.
+solo_test_() ->
+    [ {"call/2,3 executes functions correctly", ?setup(fun call/1) }
+    , {"duplicate calls are suppressed", ?setup(fun duplicate_suppression/1) }
+    ].
 
 error_handling_test_() ->
-    {"solo correctly passes through errors raised by Fun",
-     ?setup(fun exception_passthrough/0)}.
+    [ {"correctly passes through errors raised by Fun",
+       ?setup(fun exception_passthrough/1)}
+    , {"kill children that time out",
+       ?setup(fun timeouts_are_killed/1)}
+    ].
 
-exception_passthrough() ->
-    ?assertExit(badarg, solo:call(key, fun() -> erlang:exit(badarg) end)),
-    ?assertError(error, solo:call(key, fun() -> erlang:error(error) end)),
-    ?assertThrow(throw, solo:call(key, fun() -> erlang:throw(throw) end)).
+
+exception_passthrough(_) ->
+    [ ?_assertExit(badarg, solo:call(key, fun() -> erlang:exit(badarg) end))
+    , ?_assertError(error, solo:call(key, fun() -> erlang:error(error) end))
+    , ?_assertThrow(throw, solo:call(key, fun() -> erlang:throw(throw) end))
+    ].
+
+call(_) ->
+    [ %% call/2 and call/3 behaves as expected
+      ?_assertEqual(value, solo:call(key, fun() -> value end))
+    , ?_assertEqual(true, solo:call(key, fun() -> true end))
+    , ?_assertEqual(<<"xyz">>, solo:call({some, key}, fun() -> <<"xyz">> end, 5000))
+    ].
+
+timeouts_are_killed(_) ->
+    SendToSelfAndSleep = fun(Pid) ->
+				 fun() ->
+					 Pid ! self(), 
+					 timer:sleep(1000) 
+				 end
+			 end,
+    
+    [ %% Assert that solo:call exits with exit:{timeout, _} in the case where
+      %% the fun being run exceeds it's timeout
+      ?_assertExit({timeout, _}, solo:call(key, SendToSelfAndSleep(self()), 1))
+
+      %% Assert that it gets killed
+    , ?_assertNot(is_process_alive(receive_one_message(100)))
+    ].
 
 %% Spawn N many child processes, each performing a call with the same key.
 %% Ensure that the results for all children are equal.
-duplicate_suppression() ->
+duplicate_suppression(_) ->
     NumCalls = 1000, %% Number of concurrent, in-flight calls.
     Self = self(),
-    lists:foreach(fun(_) -> spawn_expensive_worker(Self) end, lists:seq(1, NumCalls)),
+    lists:foreach(fun(_) -> spawn_expensive_worker(Self, 300) end, lists:seq(1, NumCalls)),
     
     [Ref | Refs] = receive_refs(NumCalls),
     
-    lists:foldl(fun(R, R1) ->
-                        ?assertEqual(R, R1),
-                        Ref
-                end, Ref, Refs).
+    [ ?_assertEqual([Ref], lists:usort(Refs)) ].
 
 %% Receive N many refs from the process mailbox.
 receive_refs(N) ->
@@ -61,13 +80,21 @@ receive_refs(N, Acc) when N > 0 ->
             receive_refs(N-1, [Ref | Acc])
     end.
 
-spawn_expensive_worker(ParentPid) ->
+spawn_expensive_worker(ParentPid, SleepTime) ->
     %% Fun simulates a long-running call.
     Fun = fun() ->
                   Res = solo:call(key, fun() ->
-                                               timer:sleep(1000),
+                                               timer:sleep(SleepTime),
                                                erlang:make_ref()
                                        end),
                   ParentPid ! Res
           end,
     spawn_link(Fun).
+
+receive_one_message(TimeoutMs) ->
+    receive 
+	Msg ->
+	    Msg
+    after TimeoutMs ->
+	    erlang:error(timeout)
+    end.
