07e6f7898ba8ec3962968f077c01c0c5e8921dd0
Sölvi Páll Ásgeirsson <solvip@gmail.com>
2016-12-27 22:25:19 +0000

    Move num_workers discovery logic into application

--- src/solo_app.erl
+++ src/solo_app.erl
@@ -5,12 +5,24 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
+%%===================================================================
 %% Application callbacks
-%% ===================================================================
+%%===================================================================
 
 start(_StartType, _StartArgs) ->
-    solo_sup:start_link().
+    NumWorkers = application:get_env(solo, pool_size, default_num_workers()),
+    solo_sup:start_link(NumWorkers).
 
 stop(_State) ->
     ok.
+
+%%===================================================================
+%% Internal
+%%===================================================================
+
+%% @doc
+%% Default to double the number of scheduler threads
+%% @end
+-spec default_num_workers() -> pos_integer().
+default_num_workers() ->
+    erlang:system_info(schedulers_online) * 2.
--- src/solo_sup.erl
+++ src/solo_sup.erl
@@ -3,7 +3,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -15,26 +15,19 @@
 %% API functions
 %% ===================================================================
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(NumWorkers) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [NumWorkers]).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
-init([]) ->
-    NumWorkers = application:get_env(solo, pool_size, default_num_workers()),
+init([NumWorkers]) ->
     WorkerSpec = lists:map(fun worker_spec/1, lists:seq(1, NumWorkers)),
     
-    {ok, { {one_for_all, 3, 60}, [?CHILD(solo_reg, worker)] ++ WorkerSpec 
-         } }.
+    {ok, { {one_for_all, 3, 60}
+	 , [?CHILD(solo_reg, worker)] ++ WorkerSpec 
+	 }}.
 
 worker_spec(N) ->
     {N, {solo, start_link, []}, permanent, 1000, worker, [solo]}.
-
-%% @doc
-%% Default to double the number of scheduler threads
-%% @end
--spec default_num_workers() -> pos_integer().
-default_num_workers() ->
-    erlang:system_info(schedulers_online) * 2.
